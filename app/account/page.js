c7c4645323eafcd5cac2550ba12009cef4c1a22c
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-11-26 12:47:19 +0330

    Building a Custom Sign In Page

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Account() {
+export default async function Account() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl to-accent-400 mb-7">
-      Welcome Mahyar
+      Welcome {firstName}
     </h2>
   );
 }
