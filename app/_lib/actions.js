04d57dd77b8ac422015894643d3994b771f23184
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-11-30 13:13:02 +0330

    Removing Reservations Immediately The useOptimistic Hook

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -37,6 +37,9 @@ export async function signOutAction() {
 }
 
 export async function deleteReservation(bookingId) {
+  // await new Promise((res) => setTimeout(res, 5000));
+  // throw new Error("Simulated error for testing optimistic UI");
+
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
